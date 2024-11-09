03734ae911b1f9720cbc1159650579939547ac45
RaniD16 <rani.deshpande16@gmail.com>
2024-11-09 17:03:40 +0530

    Update streamlit_app.py
    final ones

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,9 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
+import requests
 import pandas as pd
+
 
 
 # Write directly to the app
@@ -25,14 +27,29 @@ ingredients_list = st.multiselect(
     ,my_dataframe
     ,max_selections=5
     )
+my_dataframe= session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
+#st.dataframe(data=my_dataframe, use_container_width=True)
+#st.stop()
 
+#Convert the Snowpark Dataframe to a Pandas Dataframe so we can use the LOC function
+pd_df=my_dataframe.to_pandas()
+#st.dataframe(pd_df)
+#st.stop()
+    
 if ingredients_list:
 
     ingredients_string=''
     for fruit_chosen in ingredients_list:
         ingredients_string +=fruit_chosen+' '
 
-    st.write(ingredients_string)
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+
+        st.subheader(fruit_chosen + 'Nutrition Information')
+        #fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_chosen)
+    
+    #st.write(ingredients_string)
+    
     
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
             values ('""" + ingredients_string + """','""" + name_on_order + """')"""
@@ -47,18 +64,23 @@ if ingredients_list:
         st.success('Your Smoothie is ordered!', icon="✅")
 
 #New Section to display fruityvice nutrition information
-import requests
+
 #fruityvice_response = requests.get("https://www.fruityvice.com/api/fruit/watermelon")
 #fruityvice_response = requests.get("https://coffee.alexflipnote.dev/random")
 #st.text(fruityvice_response.json())
 #fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
-my_dataframe= session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
-#st.dataframe(data=my_dataframe, use_container_width=True)
-#st.stop()
 
-#Convert the Snowpark Dataframe to a Pandas Dataframe so we can use the LOC function
-pd_df=my_dataframe.to_pandas()
-st.dataframe(pd_df)
-st.stop()
+
+    ingredients_list=st.multiselect(
+        'Choose up to 5 ingredients:'
+        , my_dataframe
+        , max_selection=5
+        )  
+
+    if ingredients_list:
+        ingredients_string=''
+
+
+
 
