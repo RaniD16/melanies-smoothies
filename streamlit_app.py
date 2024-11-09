bd5b61de51a130e03126f7c89db0ae282774bc60
RaniD16 <rani.deshpande16@gmail.com>
2024-11-09 16:17:15 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,5 +50,5 @@ if ingredients_list:
 import requests
 #fruityvice_response = requests.get("https://www.fruityvice.com/api/fruit/watermelon")
 fruityvice_response = requests.get("https://coffee.alexflipnote.dev/random")
-#st.text(fruityvice_response)
-fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
+st.text(fruityvice_response.json())
+#fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
