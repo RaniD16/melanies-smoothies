ead761734065f9fd2171fa365e726c6a3f622f31
RaniD16 <rani.deshpande16@gmail.com>
2024-11-09 16:52:30 +0530

    Update streamlit_app.py
    pandas

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
-
+import pandas as pd
 
 
 # Write directly to the app
@@ -52,3 +52,13 @@ import requests
 #fruityvice_response = requests.get("https://coffee.alexflipnote.dev/random")
 #st.text(fruityvice_response.json())
 #fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
+
+my_dataframe= session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
+#st.dataframe(data=my_dataframe, use_container_width=True)
+#st.stop()
+
+#Convert the Snowpark Dataframe to a Pandas Dataframe so we can use the LOC function
+pd_df=my_dataframe.to_pandas()
+st.dataframe(pd_df)
+st.stop()
+
